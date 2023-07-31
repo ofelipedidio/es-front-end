ce4b465ea67a5c14964f7fc545589ad071b11fb1
eduardo-marques8 <marquesdu2009@hotmail.com>
2023-07-31 19:24:25 -0300

    add: send and cancel buttons on the mentoria popup

--- src/app/mentores/mentores/mentores.component.ts
+++ src/app/mentores/mentores/mentores.component.ts
@@ -1,9 +1,11 @@
 import { MentorModel } from "../../models/mentores-model";
 // mentors.component.ts
 
-import { Component } from "@angular/core";
+import { Component, Input } from "@angular/core";
 import { MatTableDataSource } from "@angular/material/table";
 import { MentoresService } from "../../services/mentores.service";
+
+import { FormBuilder, FormGroup } from '@angular/forms';
 
 @Component({
   selector: "app-mentors",
@@ -15,16 +17,12 @@ export class MentoresComponent {
   dataSource: MatTableDataSource<any> = new MatTableDataSource();
   displayedColumns: string[] = ["nameRole", "tags", "action"];
 
-  constructor(private mentorService: MentoresService) {
+  constructor(private mentorService: MentoresService, private formBuilder: FormBuilder) {
     this.mentorService.getMentores().subscribe((mentors) => {
       this.dataSource = new MatTableDataSource(
         MentorModel.convertPayload(mentors)
       );
     });
-  }
-
-  onButtonClick(mentor: any) {
-    
   }
 
   applyFilter(event: Event) {
@@ -40,8 +38,30 @@ export class MentoresComponent {
 })
 export class ModalComponent {
   show: boolean = false;
+  @Input() mentoria: any;
 
-  toggle () {
+  constructor(private formBuilder: FormBuilder) {
+    this.mentoria = this.formBuilder.group({
+      duracao: '',
+      formato: '',
+      recompensa: ''
+    })
+  }
+
+  toggle() {
     this.show = !this.show;
+  }
+
+  sendAndToggle() {
+    console.log(this.mentoria.value.duracao);
+    console.log(this.mentoria.value.formato);
+    console.log(this.mentoria.value.recompensa);
+    this.mentoria.reset();
+    this.toggle();
+  }
+
+  deleteAndToggle() {
+    this.mentoria.reset();
+    this.toggle();
   }
 }
