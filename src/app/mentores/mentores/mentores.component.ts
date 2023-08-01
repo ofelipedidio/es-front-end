837195f4875686ed6b7c710b71eb0329bf9d0bd7
eduardo-marques8 <marquesdu2009@hotmail.com>
2023-07-31 21:40:51 -0300

    add: primitive solicitar mentoria

--- src/app/mentores/mentores/mentores.component.ts
+++ src/app/mentores/mentores/mentores.component.ts
@@ -1,11 +1,15 @@
 import { MentorModel } from "../../models/mentores-model";
+import { MentoriaModel } from "../../models/mentorias-model";
 // mentors.component.ts
 
 import { Component, Input } from "@angular/core";
 import { MatTableDataSource } from "@angular/material/table";
 import { MentoresService } from "../../services/mentores.service";
+import { MentoriasService } from "../../services/mentorias.service";
 
-import { FormBuilder, FormGroup } from '@angular/forms';
+import { FormBuilder } from '@angular/forms';
+import { MatSnackBar } from "@angular/material/snack-bar";
+import { Router } from "@angular/router";
 
 @Component({
   selector: "app-mentors",
@@ -39,8 +43,10 @@ export class MentoresComponent {
 export class ModalComponent {
   show: boolean = false;
   @Input() mentoria: any;
+  newMentoria: MentoriaModel = new MentoriaModel("", "", "", "", "");
 
-  constructor(private formBuilder: FormBuilder) {
+  constructor(private formBuilder: FormBuilder, private mentoriaService: MentoriasService, 
+    private snackBar: MatSnackBar, private router: Router) {
     this.mentoria = this.formBuilder.group({
       duracao: '',
       formato: '',
@@ -56,6 +62,16 @@ export class ModalComponent {
     console.log(this.mentoria.value.duracao);
     console.log(this.mentoria.value.formato);
     console.log(this.mentoria.value.recompensa);
+    this.newMentoria = new MentoriaModel("teste mentor", "teste mentorado", this.mentoria.value.duracao, 
+    this.mentoria.value.formato, this.mentoria.value.recompensa);
+    this.mentoriaService.createMentoria(this.newMentoria).subscribe(() =>
+      this.snackBar.open("Mentoria solicitada!", "Dismiss", {
+        duration: 2000,
+      })
+    );
+    this.router
+      .navigate([""], { skipLocationChange: true })
+      .then(() => this.router.navigate(["/mentorias"]));
     this.mentoria.reset();
     this.toggle();
   }
