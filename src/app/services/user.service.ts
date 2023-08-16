24a0c8fa9c4f9ad376e7d67dff3ee9d208395a5a
JoaoCosme <cosmejoaopedro@protonmail.com>
2023-08-16 19:46:11 -0300

    user session is being kept

--- src/app/services/user.service.ts
+++ src/app/services/user.service.ts
@@ -1,6 +1,7 @@
 import { UserInterface } from "./../models/user-model";
 import { Injectable } from "@angular/core";
 
+const STORED_USER_DATA_KEY = "userData";
 @Injectable({
   providedIn: "root",
 })
@@ -9,13 +10,24 @@ export class UserService {
 
   setUser(user: UserInterface) {
     this.currentUser = user;
+    localStorage.setItem(
+      STORED_USER_DATA_KEY,
+      JSON.stringify(this.currentUser)
+    );
   }
 
   getUser() {
+    if (!this.currentUser) {
+      const storedData = localStorage.getItem(STORED_USER_DATA_KEY);
+      if (storedData) {
+        this.currentUser = JSON.parse(storedData);
+      }
+    }
     return this.currentUser;
   }
 
   clearUser() {
     this.currentUser = undefined;
+    localStorage.removeItem(STORED_USER_DATA_KEY);
   }
 }
